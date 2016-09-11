b1089ef7af1d954d21bf30c1708082f7d1e3540a
Rattikarn Dudley <momokojung01@hotmail.com>
2016-09-11 19:08:16 -0400

    Finding odd or even num
    use mod to find odd and even number

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+		int num;
+
+		printf("Enter a number you want to check.\n");
+		scanf_s("%d", &num);
+		if ((num % 2) == 0)          //checking whether remainder is 0 or not.
+			printf("%d is even.", num);
+		else
+			printf("%d is odd.", num);
+
+		getc;
+		return (0);
+		
+	}
+
+
+
+
+
+
+
